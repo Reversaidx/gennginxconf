d35f22697b07d2325adbd8fc7d617bf855f3e9bb
Reversaid <reversaidx@gmail.com>
2018-04-07 10:49:37 +0800

    2

--- gennginxconf.py
+++ gennginxconf.py
@@ -51,9 +51,44 @@ if __name__ == '__main__':
  for i in hosts:
      virt_host_name=servername_re.findall(i)
      list.append(virt_host(virt_host_name))
-     virt_host.add(list[-1],serveralias_re.findall(i),serverroot.findall(i),serverip.findall(i),serverindex.findall(i))
+     virt_host.add(list[-1],serveralias_re.findall(i)[0],serverroot.findall(i)[0],serverip.findall(i)[0],serverindex.findall(i)[0])
 
-print (list[0].servername,list[0].serverroot)
+ for i in list:
+     file=open(i.servername[0]+".conf",'wt')
+     file.write('''server {
+	server_name %s %s;
+	charset off;
+	index %s;
+	disable_symlinks if_not_owner from=$root_path;
+	access_log /var/log/nginx/access.log;
+	error_log /var/log/nginx/error.log notice;
+	ssi on;
+	set $root_path %s;
+	root $root_path;
+	listen %s:80;
+	Strict-Transport-Security;
+
+	location / {
+		location ~ [^/]\.ph(p\d*|tml)$ {
+			try_files /does_not_exists @fallback;
+		}
+		location ~* ^.+\.(jpg|jpeg|gif|png|svg|js|css|mp3|ogg|mpe?g|avi|zip|gz|bz2?|rar|swf)$ {
+			try_files $uri $uri/ @fallback;
+		}
+		location / {
+			try_files /does_not_exists @fallback;
+		}
+	}
+	location @fallback {
+		proxy_pass http://127.0.0.1:8080;
+		proxy_redirect http://127.0.0.1:8080 /;
+		proxy_set_header Host $host;
+		proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+		proxy_set_header X-Forwarded-Proto $scheme;
+		proxy_set_header X-Forwarded-Port $server_port;
+		access_log off;
+	}
+}''' %(i.servername[0],i.serveralias,i.serverindex,i.serverroot,i.serverip,))
 
 
 
